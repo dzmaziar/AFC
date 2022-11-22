3408fdebd33a50952ca5722959e59c03ab43e7f2
MrTwister <artemburnovich@outlook.com>
2022-11-22 18:12:10 +0300

    Added: IEEE754 calculator

--- Main.cs
+++ Main.cs
@@ -1,12 +1,24 @@
 ﻿using AFC;
 using System;
+using System.Collections.Specialized;
 using System.Runtime.ConstrainedExecution;
 
-String pageCopy = "pageCopy.txt";
-String floats = "Combinations.txt";
-List<double> floatArr = FloatHighLighter.HighLightFloats(DAO.ReadFromFile(pageCopy));
-Console.WriteLine(String.Join("\n", floatArr));
-Calculator.DoCalculations(floatArr, 0.25, 0, 1, floats);
+//Tests
+
+//String pageCopy = "pageCopy.txt";
+//String floats = "Combinations.txt";
+//List<double> floatArr = FloatHighLighter.HighLightFloats(DAO.ReadFromFile(pageCopy));
+////Console.WriteLine(String.Join("\n", floatArr));
+////Calculator.DoCalculations(floatArr, 0.25, 0.06, 0.8, floats);
+//double req = Calculator.GetRequiredAverageFloat(0.25, 0.06, 0.8);
+//Console.WriteLine(req);
+
+//List<double> comb = new List<double>{ 0.15056592226028, 0.15334397554398, 0.15354132652283, 0.15588110685349, 0.1562362909317, 0.23051972687244, 0.3688178062439, 0.37429389357567, 0.37761142849922, 0.37918850779533 };
+//IEEE754Calculator calc = new IEEE754Calculator(comb, 0.25, 0, 1);
+//bool f = calc.DoCalculations();
+//Console.WriteLine(f);
+
+
 namespace AFC
 {  
 }
