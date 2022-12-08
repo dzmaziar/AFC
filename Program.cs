42c12f599e3448d2408c92fe88a0e91a1d327950
Dima Maziar <89743997+dzmaziar@users.noreply.github.com>
2022-12-08 17:50:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace AFC
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new StartPage());
+        }
+    }
+}
